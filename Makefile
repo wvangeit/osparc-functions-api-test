9690f153410ccb33fb61f8179841fe861eea226c
Werner Van Geit <werner.vangeit@gmail.com>
2025-04-17 10:36:03 +0200

    Moved to project_functions

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ clean:
 	rm -rf solver_files/function_code.py
 	rm -rf solver_files/tmp*
 	rm -rf solver_files/function_output.json
-
 solver_functions: clean
 	./solver_functions.sh
+project_functions: clean
+	./project_functions.sh
+
--- project_functions.sh
+++ project_functions.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+
+set -e
+
 rm -rf .venv
 uv venv .venv
 
 source .venv/bin/activate
 
-which pip
 uv pip install ../osparc-simcore-clients/clients/python/artifacts/dist/osparc_client-0.8.3.post0.dev25-py3-none-any.whl
-# uv pip install ../osparc-simcore-clients/clients/python/artifacts/dist/osparc-0.8.3.post0.dev25-py3-none-any.whl
+uv pip install ../osparc-simcore-clients/clients/python/artifacts/dist/osparc-0.8.3.post0.dev25-py3-none-any.whl
 
-python test.py
+python project_functions.py
